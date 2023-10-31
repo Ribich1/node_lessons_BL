91b450ea998e490b21c126ac33185cc386b9a65e
Ribich1 <ribich@ukr.net>
2023-10-31 21:29:25 +0100

    lessons 1

---
+++ dateFunctions/index.js
@@ -0,0 +1,1 @@
+export { default as getCurrentMonth } from "./monthFunctions/getCurrentMoth.js";
---
+++ users.js
@@ -0,0 +1,8 @@
+const admins = ["Alex", "Andrey", "Vasiliy"];
+const clients = ["Gena", "Boris", "Seva"];
+
+const users = {
+  admins,
+  clients,
+};
+export default users;
